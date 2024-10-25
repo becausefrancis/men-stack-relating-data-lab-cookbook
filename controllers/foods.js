c6e24adeb979c9b3e8ba7eb3d61c93603546ea21
becausefrancis <jimenez.francis92@gmail.com>
2024-10-25 15:28:18 -0500

    Start delete route next.

---
+++ controllers/foods.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+
+router.get('/', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      res.render('foods/index.ejs', {
+        foods: currentUser.foods,
+      });
+
+    } catch (error) {
+      console.log(error)
+      res.redirect('/')
+    }
+});
+
+router.get('/new', async (req, res) => {
+    res.render('foods/new.ejs');
+});
+
+
+
+router.post('/', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      currentUser.foods.push(req.body);
+      await currentUser.save();
+      res.redirect(`/users/${currentUser._id}/foods`);
+
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+});
+
+module.exports = router;
